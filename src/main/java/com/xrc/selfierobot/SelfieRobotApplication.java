924f6994405cc26e83d740ccc948bbea3de166fd
dzhelyazkov <d.zhelyazkov@ocado.com>
2020-08-02 22:22:04 +0300

    Revert "Disable Camera client"
    This reverts commit fe8ef3bbc585394e63d014705045c9ff89cc9d8a.

--- src/main/java/com/xrc/selfierobot/SelfieRobotApplication.java
+++ src/main/java/com/xrc/selfierobot/SelfieRobotApplication.java
@@ -3,9 +3,10 @@ package com.xrc.selfierobot;
 import gnu.io.NoSuchPortException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.xrc.*")
+@ComponentScan("com.xrc.*")
 public class SelfieRobotApplication {
 
 	public static void main(String[] args) throws NoSuchPortException {
